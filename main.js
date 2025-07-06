abda5a24e88d6a28fd3b36391d997297a6ed0af5
EmanueleIppolito <emanuele2410@gmail.com>
2025-07-06 12:57:28 +0200

    end excercise

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
-const Kilometri = prompt("Quanti Kilometri desideri percorrere?");
+const Kilometri = Number(prompt("Quanti Kilometri desideri percorrere?"));
 console.log(Kilometri);
-const anni = prompt("Quanti anni hai?")
+const anni = Number(prompt("Quanti anni hai?"));
 console.log(anni);
 let price = 0.21 * Kilometri;
 console.log(price);
 
-alert(`Il costo del tuo biglietto è di ${price} €`)
+if (anni < 18){
+    let scontoMinori = price - (price * 20 /100);
+    alert(`Il costo del tuo biglietto è di ${scontoMinori.toFixed(2)} €`);
+}
 
+else if (anni >= 65){
+    let scontoAnziani = price - (price * 40 /100);
+    alert(`Il costo del tuo biglietto è di ${scontoAnziani.toFixed(2)} €`);
+}
+
+else {
+    alert(`Il costo del tuo biglietto è di ${price.toFixed(2)} €`);
+}
