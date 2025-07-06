ce0ffc6829deca782550193c27f5c5be301a380d
EmanueleIppolito <emanuele2410@gmail.com>
2025-07-06 12:26:51 +0200

    add basic structure

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("prova")
+const Kilometri = prompt("Quanti Kilometri desideri percorrere?");
+console.log(Kilometri);
+const anni = prompt("Quanti anni hai?")
+console.log(anni);
+let price = 0.21 * Kilometri;
+console.log(price);
+
+alert(`Il costo del tuo biglietto è di ${price} €`)
+
